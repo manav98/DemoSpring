d2950039305019065701b59e40cfb37692ad0026
Manav-NTT <manav12.gupta@nttdata.com>
2024-08-22 12:34:49 +0530

    added code

--- src/main/java/com/telusko/App.java
+++ src/main/java/com/telusko/App.java
@@ -1,13 +1,16 @@
 package com.telusko;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Dev obj = (Dev) context.getBean(Dev.class);
+/*
+        obj.setAge(120);
+        System.out.println(obj.getAge());
+*/
+        obj.build();
     }
 }
